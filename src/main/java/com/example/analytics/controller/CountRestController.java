c99764fb6eaeab082f8248ae140746dcd3c2823a
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2021-01-12 21:00:52 +0000

    added the lombak and store annotations

--- src/main/java/com/example/analytics/controller/CountRestController.java
+++ src/main/java/com/example/analytics/controller/CountRestController.java
@@ -2,11 +2,14 @@ package com.example.analytics.controller;
 
 import com.example.analytics.binding.AnalyticsBinding;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsStateStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,16 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@KafkaStreamsStateStore
 public class CountRestController {
 
+  private final Log log = LogFactory.getLog(getClass());
+
   private final InteractiveQueryService interactiveQueryService;
+
+//  public CountRestController(InteractiveQueryService interactiveQueryService) {
+//    this.interactiveQueryService = interactiveQueryService;
+//  }
 
   @GetMapping("/counts")
   public Map<String, Long> counts() {
@@ -25,6 +35,8 @@ public class CountRestController {
     ReadOnlyKeyValueStore<String, Long> querableStoryType =
         this.interactiveQueryService.getQueryableStore
             (AnalyticsBinding.PAGE_COUNT_MV, QueryableStoreTypes.keyValueStore());
+
+    log.info("interactiveQueryService.getCurrentHostInfo() => " + interactiveQueryService.getCurrentHostInfo());
     KeyValueIterator<String, Long> all = querableStoryType.all();
     while (all.hasNext()) {
       KeyValue<String, Long> value = all.next();
